8d6460507518fc142fe4809a94ba05e722a82c99
Thibaut Virolle <thibaut.virolle@protonmail.com>
2025-07-10 16:38:36 +0200

    fix: test

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ import {
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
 import express from 'express';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+// import { dirname, resolve } from 'node:path';
+// import { fileURLToPath } from 'node:url';
 
-const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-const browserDistFolder = resolve(serverDistFolder, '../browser');
+//const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+//const browserDistFolder = resolve(serverDistFolder, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
@@ -29,13 +29,13 @@ const angularApp = new AngularNodeAppEngine();
 /**
  * Serve static files from /browser
  */
-app.use(
+/*app.use(
   express.static(browserDistFolder, {
     maxAge: '1y',
     index: false,
     redirect: false,
   }),
-);
+);*/
 
 /**
  * Handle all other requests by rendering the Angular application.
